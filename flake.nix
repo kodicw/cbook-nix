30def7c783a5811bcc218fa81955a041a9f5cec3
Kodi Walls <kodicw@gmail.com>
2026-04-18 08:51:37 -0700

    first commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Home Manager configuration of charles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations."charles" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+
+      modules = [ ./home.nix ];
+    };
+  };
+}
