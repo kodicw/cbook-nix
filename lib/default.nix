880e2770cf41083c9f0a6da9be6683d4ccd56902
Kodi Walls <kodicw@gmail.com>
2026-04-22 20:30:17 -0700

    fix: pass home-manager to lib for homeManagerConfiguration

--- lib/default.nix
+++ lib/default.nix
@@ -1,16 +1,13 @@
-{ nixpkgs, nixgl, polarbear, jbot }:
+{ nixpkgs, nixgl, polarbear, jbot, home-manager }:
 
 {
-  pkgs = nixpkgs.legacyPackages."x86_64-linux";
-  extraSpecialArgs = { inherit nixgl polarbear jbot; };
-
   makeUserConfig = sharedModules: username: extraModules:
     let
       userModule = import ../config/users/${username}.nix;
     in
-    nixpkgs.lib.homeManagerConfiguration {
-      pkgs = pkgs;
-      extraSpecialArgs = extraSpecialArgs;
+    home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      extraSpecialArgs = { inherit nixgl polarbear jbot; };
       modules = sharedModules ++ extraModules ++ [
         { _module.args = { inherit userModule; }; }
       ];
